a0d0920cc071291d55e0a2090c6ce94dfd77619f
0x014f <olaf.gaza@outlook.com>
2024-01-07 12:43:59 +0100

    router - initial commit

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-console.log(
-  "this is the simple app using just the vanilla js - so just the javascript without any framework on top of that"
-);
+import Router from "./services/router.js";
+
+window.addEventListener("DOMContentLoaded", startTheApp);
+
+function startTheApp() {
+  console.log(
+    "this is the simple app using just the vanilla js - so just the javascript without any framework on top of that"
+  );
+}
