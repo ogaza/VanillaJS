7369f538b72865d00091e6a13239e3082fd7a1ed
Olaf Gaża <olaf.gaza.fp@gmail.com>
2024-01-08 18:29:25 +0100

    router - some commnets on the ways in which we can change the application's page content

--- services/router.js
+++ services/router.js
@@ -1,3 +1,15 @@
 const Router = {};
 
 export default Router;
+
+// there are two ways of changing the page's content
+// one is to remove one content and put a new content
+// to the DOM based on user's actions
+// the other way is to show one part and hide the other parts
+// using the hidden attribute like
+// <section id="sectionOne"> ...
+// <section id="sectionTwo" hidden> ...
+// which is better?
+// it depends for example on the size of the application
+// its better to show/hide only in small ones
+// but do we always write big applications?
