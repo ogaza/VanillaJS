162c3069cd1afbd055854475e874618d91b4305a
Olaf Gaża <olaf.gaza.fp@gmail.com>
2024-02-02 17:27:32 +0100

    funcLightJS_v3: list operations #1

---
+++ funcLightJS_v3/listOperations/index.js
@@ -0,0 +1,74 @@
+const title = "FuncJS - " + "list operations";
+document.querySelector("h1").textContent = title;
+document.title = title;
+
+const five = constant(5);
+const nine = constant(9);
+
+console.log(addn([constant(2), constant(8), five, five, nine, constant(1)]));
+// console.log(addn(constant(2), constant(8), five, five, nine, constant(1)));
+function constant(n) {
+  return function f() {
+    return n;
+  };
+}
+
+// the reduction approach
+function addn(fns) {
+  const composedFn = fns.reduce(function reducer(bigFn, fn) {
+    return function f() {
+      return add2(bigFn, fn);
+    };
+  });
+
+  return composedFn();
+}
+
+// recursive approach
+// notice here how the recursive call
+// fill the Proper Tail Call Position
+// requirement, so this recursion can be
+// optimized by the JS engine
+// function addn([fn0, fn1, ...rest]) {
+//   if (rest.length === 0) {
+//     return add2(fn0, fn1);
+//   }
+//   // recursive call is on an array smaller by one elem
+//   return addn([
+//     function f() {
+//       return add2(fn0, fn1);
+//     },
+//     ...rest
+//   ]);
+// }
+
+// deffer the job with functions approach
+// function addn(...fns) {
+//   while (fns.length > 2) {
+//     let [fn0, fn1, ...rest] = fns;
+
+//     fns = [
+//       function f() {
+//         return add2(fn0, fn1);
+//       },
+//       ...rest
+//     ];
+//   }
+
+//   return add2(fns[0], fns[1]);
+// }
+
+function add2(fn1, fn2) {
+  return add(fn1(), fn2());
+}
+
+function add(x, y) {
+  return x + y;
+}
+
+// function five() {
+//   return 5;
+// }
+// function nine() {
+//   return 9;
+// }
