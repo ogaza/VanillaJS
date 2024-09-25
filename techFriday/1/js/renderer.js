c21699e4d9246688fe7648ac31dc6b086019577f
Olaf Gaża <olaf.gaza.fp@gmail.com>
2024-09-25 13:47:16 +0200

    techFridayOne: initial handling the locationChanged event

--- techFriday/1/js/renderer.js
+++ techFriday/1/js/renderer.js
@@ -1,7 +1,14 @@
 const Renderer = {
   init: function () {
     console.log("renderer init function");
+    // handle location changed
+    window.addEventListener("locationChanged", handleWindowLocationChanged);
   }
 };
 
 export default Renderer;
+
+function handleWindowLocationChanged(event) {
+  const route = location.pathname;
+  console.log(`window location changed to: ${route}`);
+}
