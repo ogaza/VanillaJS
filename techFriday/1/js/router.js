6275ff573b43a3bf0c88e5ff262bf00f167469bd
Olaf Gaża <olaf.gaza.fp@gmail.com>
2024-09-25 13:28:33 +0200

    techFridayOne: app router - switch off anchors

--- techFriday/1/js/router.js
+++ techFriday/1/js/router.js
@@ -1,7 +1,19 @@
 const Router = {
   init: function () {
     console.log("router init function");
+
+    // use dom api to change the behaviour of all navigation links
+    // taggeg with a specific class
+    document.querySelectorAll("a.navlink").forEach(modifyAnchorBehaviour);
   }
 };
 
 export default Router;
+
+function modifyAnchorBehaviour(anchor) {
+  anchor.addEventListener("click", handleAnchorClicked);
+}
+
+function handleAnchorClicked(event) {
+  event.preventDefault();
+}
