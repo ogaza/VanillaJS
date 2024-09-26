afde59d56976c4f40c65bccc9daa161e30150efd
Olaf Gaża <olaf.gaza.fp@gmail.com>
2024-09-26 08:30:32 +0200

    techFridayOne: renderer - define default route view and implement matching routes to views

--- techFriday/1/js/renderer.js
+++ techFriday/1/js/renderer.js
@@ -8,7 +8,31 @@ const Renderer = {
 
 export default Renderer;
 
+const routeViewRenderers = {
+  routeNotFound: pageNotFoundView
+};
+
 function handleWindowLocationChanged(event) {
+  renderMainView();
+}
+
+function renderMainView() {
   const route = location.pathname;
-  console.log(`window location changed to: ${route}`);
+  const mainHtmlElem = document.querySelector("main");
+  const renderFunction = matchRouteToView(route);
+  renderFunction(mainHtmlElem);
+}
+
+function matchRouteToView(route) {
+  const rendererKey =
+    Object.keys(routeViewRenderers).find((x) => x.includes(route)) ??
+    "routeNotFound";
+
+  const viewRenderer = routeViewRenderers[rendererKey];
+
+  return viewRenderer;
+}
+
+function pageNotFoundView(parentNode) {
+  parentNode.textContent = "Page not found";
 }
