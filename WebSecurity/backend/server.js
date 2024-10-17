b5df9979d0772cda9df3dc2a2dae80839084a648
Olaf Gaża <olaf.gaza.fp@gmail.com>
2024-10-17 14:46:23 +0200

    WebSecurity: config retrieval

--- WebSecurity/backend/server.js
+++ WebSecurity/backend/server.js
@@ -1,9 +1,12 @@
-import express from "express";
 import dotenv from "dotenv";
+import express from "express";
 
 // dotenv enables the usage of the .env file
 dotenv.config();
-const PORT = process.env.PORT || 3000;
+
+const {
+  env: { PORT: port = 3030 }
+} = process;
 
 const app = express();
 
@@ -11,6 +14,6 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(PORT, () => {
-  console.log(`web-security app's backend part is listening on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`web-security app's backend part is listening on port ${port}`);
 });
