e618856c9592982989f6c5b5f311600d90cdf7c5
Olaf Gaża <olaf.gaza.fp@gmail.com>
2024-10-18 10:29:51 +0200

    WebSecurity: client app on a separate node process

---
+++ WebSecurity/frontend/server.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.static("./"));
+
+const port = 5501;
+
+app.listen(port, () => {
+  console.log(`web-security app's frontend part is listening on port ${port}`);
+});
