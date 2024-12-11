299c24fbb963ac82102bb3e9fc6b7153b2798e6e
Olaf Gaża <olaf.gaza.fp@gmail.com>
2024-12-11 11:18:36 +0100

    WebSecurity: basic CSP configuration with 'helmet'

--- WebSecurity/backend/server.js
+++ WebSecurity/backend/server.js
@@ -1,5 +1,6 @@
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
+import helmet from "helmet";
 import express from "express";
 import { appConfig, cookiesConfig } from "./config.js";
 import { useMyApi } from "./api/api.js";
@@ -10,6 +11,18 @@ const { port } = appConfig;
 
 const app = express();
 
+// Use Helmet to set security headers
+app.use(helmet());
+// Setting up CSP
+app.use(
+  helmet.contentSecurityPolicy({
+    useDefaults: true,
+    directives: {
+      defaultSrc: ["'self'"],
+      scriptSrc: ["'self'"]
+    }
+  })
+);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 if (cookiesConfig.enabled) {
